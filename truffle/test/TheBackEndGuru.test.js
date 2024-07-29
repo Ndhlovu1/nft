7c26563a62d2a166da9c1ef24d632e4aac4cc641
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-07-29 18:29:19 +0200

    Added (core) : Successfully running truffle to create migrations

--- truffle/test/TheBackEndGuru.test.js
+++ truffle/test/TheBackEndGuru.test.js
@@ -1,4 +1,6 @@
 const TBeg = artifacts.require("TheBackEndGuru")
+const truffleAssert = require("truffle-assertions")
+
 
 // USE CONTRACT INSTEAD OF MOCHA
 contract("TheBackEndGuru",(accounts) => {
@@ -6,10 +8,21 @@ contract("TheBackEndGuru",(accounts) => {
     it("Credit an NFT to a specific account", async () => {
         const TbegInstance = await TBeg.deployed()
 
-        await TbegInstance.safeMint(accounts[1], "TheBackEndGuru.json")
+        let TxResult = await TbegInstance.safeMint(accounts[1], "TheBackEndGuru.json")
 
-        console.log(await TbegInstance.ownerOf(0))
+        //VERIFYING LOGS
+        truffleAssert.eventEmitted(TxResult, "Transfer", {from: '0x0000000000000000000000000000000000000000', to: accounts[1], tokenId: web3.utils.toBN("0") })
+
+        //console.log(TxResult.logs[0].args)
+
+        assert.equal(await TbegInstance.ownerOf(0), accounts[1]), "Owner of Token 1 is not Equal to 2nd Account"
+
+        //console.log(await TbegInstance.ownerOf(0))
+
+
     })
+
+
 
 
 })
--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -59,8 +59,8 @@ module.exports = {
 
 // networks: {
 //     development: {
-//       host: "localhost",
-//       port: 7545,
+//       host: "ganache",
+//       port: 8545,
 //       network_id: "5777",
 //     }
 //     // live: { ... }
